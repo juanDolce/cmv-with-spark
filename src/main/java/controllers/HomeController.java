36eff6c4e57efc7ffb5a53e33776be2cedebbce8
juan dolce <jdolce@frba.utn.edu.ar>
2023-09-20 21:53:03 -0300

    Introduccion a CMV con spark

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import spark.Request;
+import spark.Response;
+
+
+public class HomeController {
+  public Void index(Request request, Response response) {
+    response.redirect("/bienvenida");
+    return null;
+  }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+import controllers.HomeController;
+import controllers.SaludoController;
+
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Spark.port(9000);
+    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+
+
+    HomeController home = new HomeController();
+    SaludoController saludo = new SaludoController();
+
+
+    Spark.get("/", home::index);
+    Spark.get("/bienvenida", saludo::bienvenida, engine);
+    Spark.get("/despedida", saludo::despedida, engine);
+  }
+}
